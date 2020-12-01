640bbd3bca9c66150159ce52633ce488b3498f8b
DazWilkin <daz.wilkin@gmail.com>
2020-12-01 10:48:46 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,157 @@
+use pest::Parser;
+use pest_derive::*;
+
+#[derive(Parser)]
+#[grammar = "zeroconf.pest"]
+pub struct ZeroConfParser;
+
+fn main() {
+    good();
+    // bad();
+}
+fn good() {
+    let tcp = "tcp";
+    let rust = "rust";
+    let kind = format!("_{}._{}", rust, tcp);
+    let full_kind = format!("kind=\"{}\"", &kind);
+
+    let domain = "local";
+    let full_domain = format!("domain=\"{}\"", &domain);
+    let successful_parse = ZeroConfParser::parse(Rule::full_domain, &full_domain);
+    println!("{:?}", successful_parse);
+
+    let filter = format!("{} {}", full_domain, full_kind);
+    let successful_parse = ZeroConfParser::parse(Rule::filter, &filter);
+    println!("{:?}", successful_parse);
+
+    let filter = format!("{} {}", full_kind, full_domain);
+    let successful_parse = ZeroConfParser::parse(Rule::filter, &filter);
+    println!("{:?}", successful_parse);
+
+    // But permits duplicates
+    let filter = format!("{} {}", full_kind, full_kind);
+    let successful_parse = ZeroConfParser::parse(Rule::filter, &filter);
+    println!("{:?}", successful_parse);
+}
+
+fn bad() {
+    let unsuccessful_parse = ZeroConfParser::parse(Rule::protocol, "dns");
+    println!("{:?}", unsuccessful_parse);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ZeroConfParser;
+    use super::*;
+    use pest::{consumes_to, parses_to};
+
+    const TCP: &str = "tcp";
+    const RUST: &str = "rust";
+    const DOMAIN: &str = "local";
+
+    #[test]
+    fn test_tcp() {
+        parses_to! {
+            parser:ZeroConfParser,
+            input:TCP,
+            rule: Rule::tcp,
+            tokens:[
+                tcp(0,3)
+            ]
+        };
+    }
+    #[test]
+    fn test_protcol() {
+        parses_to! {
+            parser:ZeroConfParser,
+            input:TCP,
+            rule: Rule::protocol,
+            tokens:[
+                protocol(0,3,[
+                    tcp(0,3)
+                    ])
+            ]
+        };
+    }
+    #[test]
+    fn test_stype() {
+        parses_to! {
+            parser:ZeroConfParser,
+            input:RUST,
+            rule:Rule::stype,
+            tokens:[
+                stype(0,4)
+            ]
+        }
+    }
+    #[test]
+    fn test_kind() {
+        let kind: String = format!("_{}._{}", RUST, TCP);
+        parses_to! {
+            parser:ZeroConfParser,
+            input:&kind,
+            rule:Rule::kind,
+            tokens:[
+                kind(0,10,[
+                    full_stype(0,5,[
+                        stype(1,5)
+                    ]),
+                    full_protocol(6,10,[
+                        protocol(7,10,[
+                            tcp(7,10)
+                        ])
+                    ])
+                ])
+            ]
+        }
+    }
+    #[test]
+    fn test_full_kind() {
+        let kind: String = format!("_{}._{}", RUST, TCP);
+        let full_kind = format!("kind=\"{}\"", &kind);
+        parses_to! {
+            parser:ZeroConfParser,
+            input:&full_kind,
+            rule:Rule::full_kind,
+            tokens:[
+                full_kind(0,17,[
+                    kind(6,16,[
+                        full_stype(6,11,[
+                            stype(7,11)
+                        ]),
+                        full_protocol(12,16,[
+                            protocol(13,16,[
+                                tcp(13,16)
+                            ])
+                        ])
+                    ])
+                ])
+            ]
+        }
+    }
+    #[test]
+    fn test_domain() {
+        parses_to! {
+            parser:ZeroConfParser,
+            input:DOMAIN,
+            rule: Rule::domain,
+            tokens:[
+                domain(0,5)
+            ]
+        };
+    }
+    #[test]
+    fn test_full_domain() {
+        let full_domain = format!("domain=\"{}\"", DOMAIN);
+        parses_to! {
+            parser:ZeroConfParser,
+            input:&full_domain,
+            rule: Rule::full_domain,
+            tokens:[
+                full_domain(0,14,[
+                    domain(8,13)
+                ])
+            ]
+        };
+    }
+}
