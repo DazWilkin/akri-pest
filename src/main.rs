d000f1f9f328bdcacbe7ee67144336c75c90f80a
DazWilkin <daz.wilkin@gmail.com>
2020-12-01 10:57:50 -0800

    Using `lazy_static!`

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,6 @@ fn good() {
 
     let domain = "local";
     let full_domain = format!("domain=\"{}\"", &domain);
-    let successful_parse = ZeroConfParser::parse(Rule::full_domain, &full_domain);
-    println!("{:?}", successful_parse);
 
     let filter = format!("{} {}", full_domain, full_kind);
     let successful_parse = ZeroConfParser::parse(Rule::filter, &filter);
@@ -34,20 +32,22 @@ fn good() {
     println!("{:?}", successful_parse);
 }
 
-fn bad() {
-    let unsuccessful_parse = ZeroConfParser::parse(Rule::protocol, "dns");
-    println!("{:?}", unsuccessful_parse);
-}
-
 #[cfg(test)]
 mod tests {
     use super::ZeroConfParser;
     use super::*;
+    use lazy_static::*;
     use pest::{consumes_to, parses_to};
 
     const TCP: &str = "tcp";
     const RUST: &str = "rust";
     const DOMAIN: &str = "local";
+
+    lazy_static! {
+        static ref KIND: String = format!("_{}._{}", RUST, TCP);
+        static ref FULL_KIND: String = format!("kind=\"{}\"", format!("_{}._{}", RUST, TCP));
+        static ref FULL_DOMAIN: String = format!("domain=\"{}\"", DOMAIN);
+    }
 
     #[test]
     fn test_tcp() {
@@ -86,10 +86,9 @@ mod tests {
     }
     #[test]
     fn test_kind() {
-        let kind: String = format!("_{}._{}", RUST, TCP);
         parses_to! {
             parser:ZeroConfParser,
-            input:&kind,
+            input:&KIND,
             rule:Rule::kind,
             tokens:[
                 kind(0,10,[
@@ -107,11 +106,9 @@ mod tests {
     }
     #[test]
     fn test_full_kind() {
-        let kind: String = format!("_{}._{}", RUST, TCP);
-        let full_kind = format!("kind=\"{}\"", &kind);
         parses_to! {
             parser:ZeroConfParser,
-            input:&full_kind,
+            input:&FULL_KIND,
             rule:Rule::full_kind,
             tokens:[
                 full_kind(0,17,[
@@ -142,10 +139,9 @@ mod tests {
     }
     #[test]
     fn test_full_domain() {
-        let full_domain = format!("domain=\"{}\"", DOMAIN);
         parses_to! {
             parser:ZeroConfParser,
-            input:&full_domain,
+            input:&FULL_DOMAIN,
             rule: Rule::full_domain,
             tokens:[
                 full_domain(0,14,[
